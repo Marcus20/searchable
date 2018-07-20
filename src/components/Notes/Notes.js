fc11834d84ecbf3a298a41d6354710270d9dfe4c
Marcus20 <marcusquarles20@gmail.com>
2018-07-20 14:33:01 -0400

    add placeholder and update input bgcolor

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -20,6 +20,7 @@ export default class Notes extends Component {
                     style={styles.inputBox}
                     onChangeText={text => this.setState({searchText: text})}
                     value={this.state.searchText}
+                    placeholder={'Enter a team name'}
                 />
 
                 <List 
@@ -34,7 +35,9 @@ export default class Notes extends Component {
 const styles = StyleSheet.create({
     inputBox: {
       height: 40,
-      backgroundColor: 'yellow',
+      backgroundColor: '#f7f0d9',
       marginTop: 20,
+      color: '#000',
+      paddingLeft: 10,
     },
 });
